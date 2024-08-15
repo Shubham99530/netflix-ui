e33614beb5175f8ad5122fcb6cd6bebd8d341c6f
Shubham99530 <shubham21290@iiitd.ac.in>
2024-08-15 14:55:06 +0530

    new files

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7iFXspHWbelmSKGJ8uG7eC6lkLXb6gKg",
+  authDomain: "netflix-clone-37251.firebaseapp.com",
+  projectId: "netflix-clone-37251",
+  storageBucket: "netflix-clone-37251.appspot.com",
+  messagingSenderId: "312409645025",
+  appId: "1:312409645025:web:958fcc9e877d8b8252bca0",
+  measurementId: "G-Z9VPMSRS9Y"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
